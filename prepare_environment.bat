e322ba5b9c69382f1f33beb5dcb15d7e8120c1ab
vardanyangag <70974047+vardanyangag@users.noreply.github.com>
2024-02-13 18:19:25 +0400

    Update prepare_environment.bat

--- prepare_environment.bat
+++ prepare_environment.bat
@@ -1,11 +1,3 @@
-if not exist venv (
-    echo Creating virtual environment...
-    python -m venv venv
-)
-
-echo Activating virtual environment...
-call venv\Scripts\activate
-
 
 echo Installing required libraries...
 pip install requests
